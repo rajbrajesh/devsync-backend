eae091c3f80a429b7e77f76961421eff0b54f6a7
Brajesh Raj <aec.cse.rajbrajesh@gmail.com>
2026-03-12 21:06:08 +0530

    Day_14 - Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Root component of DevSync frontend
+// This component loads the main UI of our application
+
+function App() {
+
+  return (
+    <div style={{padding:"40px"}}>
+
+      {/* Application Title */}
+      <h1>DevSync Dashboard</h1>
+
+      {/* Short description */}
+      <p>Track coding problems across multiple platforms</p>
+
+    </div>
+  )
+
+}
+
+export default App
